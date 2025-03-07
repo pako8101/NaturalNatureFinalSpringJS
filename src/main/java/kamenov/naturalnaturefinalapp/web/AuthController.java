b43660f365a7e53cfdcd4e0a8de11a5ad5fe9780
pako8101 <pako810129@gmail.com>
2025-03-07 22:29:46 +0200

    controllers and html css

--- src/main/java/kamenov/naturalnaturefinalapp/web/AuthController.java
+++ src/main/java/kamenov/naturalnaturefinalapp/web/AuthController.java
@@ -38,8 +38,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.HashMap;
-import java.util.Map;
+
 
 @Controller
 @RequestMapping("/user")
